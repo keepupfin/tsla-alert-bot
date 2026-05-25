1936eb5be686bfdc2261e9bcd94c455248d1106b
keepupfin <tomerduynov1@gmail.com>
2026-05-25 20:00:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ta
 import requests
 import time
 
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+TOKEN = "8392925173:AAHF0DUwzoL1fXGDPJwXLmvASJSuVOZk1X4"
 CHAT_ID = "1702453586"
 
 def send_telegram_message(message):
@@ -19,12 +19,13 @@ def send_telegram_message(message):
 
 
 while True:
-    data = yf.download("TSLA", period="3mo", interval="1d")
+    data = yf.download("TSLA", period="1y", interval="1d")
 
     close_prices = data["Close"].squeeze()
 
     data["EMA20"] = ta.trend.EMAIndicator(close_prices, window=20).ema_indicator()
     data["EMA50"] = ta.trend.EMAIndicator(close_prices, window=50).ema_indicator()
+    data["EMA100"] = ta.trend.EMAIndicator(close_prices, window=100).ema_indicator()
     data["RSI"] = ta.momentum.RSIIndicator(close_prices, window=14).rsi()
 
     latest = data.iloc[-1]
@@ -32,14 +33,16 @@ while True:
     close = float(latest["Close"])
     ema20 = float(latest["EMA20"])
     ema50 = float(latest["EMA50"])
+    ema100 = float(latest["EMA100"])
     rsi = float(latest["RSI"])
 
     print("\nTSLA Price:", round(close, 2))
     print("EMA20:", round(ema20, 2))
     print("EMA50:", round(ema50, 2))
+    print("EMA100:", round(ema100, 2))
     print("RSI:", round(rsi, 2))
 
-    if close > ema20 and ema20 > ema50 and rsi > 50 and rsi < 70:
+    if close > ema20 and ema20 > ema50 and ema50 > ema100 and rsi > 50 and rsi < 70:
         print("BUY SIGNAL")
         send_telegram_message("🚀 BUY SIGNAL for TSLA")
 
@@ -51,4 +54,5 @@ while True:
         print("NO CLEAR SIGNAL")
         send_telegram_message("No clear signal for TSLA")
 
+    time.sleep(3600)
     time.sleep(3600)
